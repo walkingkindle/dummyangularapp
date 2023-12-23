657196a31d89279f36851a63d6d6c8de8d4f9d2c
Aleksa Hadzic <aki.hadza@gmail.com>
2023-12-23 19:58:12 +0100

    adding improvements

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -7,7 +7,7 @@ import { FormControl, FormGroup,FormsModule, ReactiveFormsModule, Validators } f
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css'],
-  imports:[FormsModule,ReactiveFormsModule]
+  imports:[FormsModule,ReactiveFormsModule,CommonModule]
 })
 export class UserRegisterComponent implements OnInit {
   public registrationForm!: FormGroup;
@@ -16,13 +16,28 @@ export class UserRegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registrationForm = new FormGroup({
-      userName:new FormControl('',Validators.required),
-      email: new FormControl(null,[Validators.email])
-    })
+      userName:new FormControl(null,Validators.required),
+      email: new FormControl(null,[Validators.email, Validators.required]),
+      password: new FormControl(null,[Validators.required,Validators.minLength(8)]),
+      confirmPassword: new FormControl(null,[Validators.required]),
+      mobile:new FormControl(null,[Validators.required, Validators.maxLength(10)]),
+    }
+    );
   }
 
   onSubmit(){
-    console.log(this.registrationForm)
+    console.log(this.registrationForm.value)
+
+  }
+  get userName(){
+    return this.registrationForm.get('userName') as FormControl;
+  }
+  get password(){
+    return this.registrationForm.get('password') as FormControl;
+  }
+
+  get confirmPassword(){
+    return this.registrationForm.get('confirmPassword') as FormControl;
   }
 
 }
